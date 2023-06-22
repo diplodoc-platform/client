ee44da35168cbbaabd93a87e699b6ddb2b7a4c52
3y3k0 <3y3k0@yandex-team.ru>
2023-06-23 02:58:42 +0300

    fix: Optimize build size

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
 const {resolve} = require('path');
 const {DefinePlugin} = require('webpack');
-// const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer');
 
 function config({isServer}) {
     return {
         mode: 'production',
         target: isServer ? 'node' : 'web',
-        devtool: 'inline-source-map',
+        devtool: 'source-map',
         entry: './src/index.tsx',
         output: {
             path: resolve(__dirname, 'build'),
@@ -16,8 +17,8 @@ function config({isServer}) {
             } : {})
         },
         resolve: {
-            alias: isServer ? {} : {
-                react: require.resolve('react'),
+            alias: {
+                'react': require.resolve('react'),
             },
             extensions: ['.tsx', '.ts', '.js', '.scss'],
         },
@@ -31,8 +32,12 @@ function config({isServer}) {
             //     analyzerMode: 'static',
             //     openAnalyzer: false,
             //     reportFilename: (isServer ? 'server-' : 'client-') + 'stats.html',
-            // })
-        ],
+            // }),
+            new MiniCSSExtractPlugin({
+                filename: 'app.client.css',
+                chunkFilename: 'app.client.css',
+            }),
+        ].filter(Boolean),
         module: {
             rules: [
                 {
@@ -45,32 +50,7 @@ function config({isServer}) {
                 }, {
                     test: /\.s?css$/,
                     use:  [
-                        {
-                            loader: 'style-loader',
-                            options: {
-                                insert: function insertBeforeAt(element) {
-                                    /* eslint-env browser */
-                                    const parent = document.querySelector('head');
-                                    const target = document.querySelector('#custom-style');
-
-                                    const lastInsertedElement =
-                                        window._lastElementInsertedByStyleLoader;
-
-                                    if (!lastInsertedElement) {
-                                        parent.insertBefore(element, target);
-                                    } else if (lastInsertedElement.nextSibling) {
-                                        parent.insertBefore(
-                                            element,
-                                            lastInsertedElement.nextSibling,
-                                        );
-                                    } else {
-                                        parent.appendChild(element);
-                                    }
-
-                                    window._lastElementInsertedByStyleLoader = element;
-                                },
-                            },
-                        },
+                        MiniCSSExtractPlugin.loader,
                         {loader: 'css-loader'},
                         {loader: 'sass-loader'},
                     ],
