6a4a84a3be77f4ce0f7aab1cd2b4b4a3d7778beb
3y3k0 <3y3k0@yandex-team.ru>
2023-06-25 01:06:22 +0300

    fix: Export more types

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,4 @@
-import React, {ReactElement, useCallback, useEffect, useState} from 'react';
+import React, { ReactElement, useCallback, useEffect, useState } from 'react';
 
 import {
     DocLeadingPage,
@@ -10,12 +10,12 @@ import {
     TextSizes,
     Theme,
 } from '@doc-tools/components';
-import {getDocSettings, updateRootClassName, withSavingSetting} from '../../utils';
+import { getDocSettings, updateRootClassName, withSavingSetting } from '../../utils';
 
 import '../../interceptors/leading-page-links';
 
-import {useMermaid} from '@diplodoc/mermaid-extension/hooks';
-import {Runtime as OpenapiSandbox} from '@diplodoc/openapi-extension/runtime';
+import { useMermaid } from '@diplodoc/mermaid-extension/hooks';
+import { Runtime as OpenapiSandbox } from '@diplodoc/openapi-extension/runtime';
 import '@diplodoc/openapi-extension/runtime/styles';
 
 import '@doc-tools/components/styles/themes.scss';
@@ -24,31 +24,29 @@ import '@doc-tools/components/styles/typography.scss';
 import '@doc-tools/transform/dist/css/yfm.css';
 import './App.scss';
 
-export interface DocProps {
-    data: DocLeadingPageData | DocPageData;
-}
-
 export interface AppProps {
     lang: Lang;
     router: Router;
 }
 
-export type DocInnerProps =
-    & DocProps
+export type DocInnerProps<Data = DocLeadingPageData | DocPageData> =
+    & { data: Data }
     & AppProps;
+
+export type { DocLeadingPageData, DocPageData };
 
 const MOBILE_VIEW_WIDTH_BREAKPOINT = 900;
 
 export function App(props: DocInnerProps): ReactElement {
-    const {data, router, lang} = props;
+    const { data, router, lang } = props;
 
     const docSettings = getDocSettings();
-    const [isMobileView, setIsMobileView] = useState(typeof document !== 'undefined' && document.body.clientWidth <= MOBILE_VIEW_WIDTH_BREAKPOINT);
-    const [wideFormat, setWideFormat] = useState(docSettings.wideFormat);
-    const [fullScreen, setFullScreen] = useState(docSettings.fullScreen);
-    const [showMiniToc, setShowMiniToc] = useState(docSettings.showMiniToc);
-    const [theme, setTheme] = useState(docSettings.theme);
-    const [textSize, setTextSize] = useState(docSettings.textSize);
+    const [ isMobileView, setIsMobileView ] = useState(typeof document !== 'undefined' && document.body.clientWidth <= MOBILE_VIEW_WIDTH_BREAKPOINT);
+    const [ wideFormat, setWideFormat ] = useState(docSettings.wideFormat);
+    const [ fullScreen, setFullScreen ] = useState(docSettings.fullScreen);
+    const [ showMiniToc, setShowMiniToc ] = useState(docSettings.showMiniToc);
+    const [ theme, setTheme ] = useState(docSettings.theme);
+    const [ textSize, setTextSize ] = useState(docSettings.textSize);
     const pageProps = {
         router,
         lang,
@@ -77,24 +75,24 @@ export function App(props: DocInnerProps): ReactElement {
 
     useEffect(() => {
         updateRootClassName(theme, isMobileView);
-    }, [theme, isMobileView]);
+    }, [ theme, isMobileView ]);
 
     const renderMermaid = useMermaid();
     useEffect(() => {
         renderMermaid({
             theme: theme === Theme.Dark ? 'dark' : 'neutral',
         });
-    }, [theme, renderMermaid]);
+    }, [ theme, renderMermaid ]);
 
     return (
         // TODO(vladimirfedin): Replace Layout__content class.
         <div className="App Layout__content">
-            {data.leading
-                ? <DocLeadingPage {...data} {...pageProps}/>
+            { data.leading
+                ? <DocLeadingPage { ...data } { ...pageProps }/>
                 // @ts-ignore
-                : <DocPage {...data} {...pageProps}/>
+                : <DocPage { ...data } { ...pageProps }/>
             }
-            <OpenapiSandbox />
+            <OpenapiSandbox/>
         </div>
     );
 }
