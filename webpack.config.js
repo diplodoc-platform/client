8af2addf51edc59dfc8433ee6fed6b97000400d7
martyanov-av <martyanov-av@yandex-team.ru>
2024-02-28 17:22:03 +0300

    fix: css manifest order

--- webpack.config.js
+++ webpack.config.js
@@ -83,9 +83,11 @@ function config({isServer, isDev, analyze = false}) {
                     const name = ({name}) => name;
                     const endsWith = (tail) => ({name}) => name.endsWith(tail);
                     const runtimeLast = (a, b) => b.chunk.id - a.chunk.id;
+                    const appLast = (a, b) => a.includes('app.') - b.includes('app.')
+
                     return {
                         js: files.filter(endsWith('.js')).sort(runtimeLast).map(name),
-                        css: files.filter(endsWith('.css')).sort(runtimeLast).map(name),
+                        css: files.filter(endsWith('.css')).sort(runtimeLast).map(name).sort(appLast),
                     };
                 }
             }),
