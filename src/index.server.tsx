bd7a97210eedd2cf3eaccf990426421b35f998a1
martyanov-av <martyanov-av@yandex-team.ru>
2024-03-24 20:01:26 +0300

    add pc & leading page link key names

--- src/index.server.tsx
+++ src/index.server.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import {renderToString} from 'react-dom/server';
 
 import {App, DocInnerProps, DocLeadingPageData, DocPageData} from './components/App/App';
+import {LINK_KEYS} from './constants';
 
 export type {DocInnerProps, DocPageData, DocLeadingPageData};
+export {LINK_KEYS};
 
 export const render = (props: DocInnerProps) => renderToString(<App {...props} />);
