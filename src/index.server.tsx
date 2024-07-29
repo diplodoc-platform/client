daac7c9b1c39e24d8afdb7a5a181f3e162af089a
Georgy Malkov <malkovgv@yandex-team.ru>
2024-07-29 17:50:18 +0300

    feat: add consent popup support (#55)
    * feat: add consent popup support
    
    * fix analytics show

--- src/index.server.tsx
+++ src/index.server.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import {renderToString} from 'react-dom/server';
 
-import {App, DocInnerProps, DocLeadingPageData, DocPageData} from './components/App/App';
+import {
+    App,
+    DocAnalytics,
+    DocInnerProps,
+    DocLeadingPageData,
+    DocPageData,
+} from './components/App/App';
 import {LINK_KEYS, LINK_KEYS_LEADING_CONFIG, LINK_KEYS_PAGE_CONSTRUCTOR_CONFIG} from './constants';
 import {preprocess} from './preprocess';
 
-export type {DocInnerProps, DocPageData, DocLeadingPageData};
+export type {DocInnerProps, DocPageData, DocLeadingPageData, DocAnalytics};
 export {LINK_KEYS, LINK_KEYS_LEADING_CONFIG, LINK_KEYS_PAGE_CONSTRUCTOR_CONFIG, preprocess};
 
 export const render = (props: DocInnerProps) => renderToString(<App {...props} />);
