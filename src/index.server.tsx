e2b9f19f7aa0c3396431bd3a95c5275c83d1ef27
martyanov-av <martyanov-av@yandex-team.ru>
2024-11-12 17:33:12 +0300

    fix: export ConfigData&PreloadParams types

--- src/index.server.tsx
+++ src/index.server.tsx
@@ -3,9 +3,16 @@ import {renderToString} from 'react-dom/server';
 
 import {App, DocAnalytics, DocInnerProps, DocLeadingPageData, DocPageData} from './components/App';
 import {LINK_KEYS, LINK_KEYS_LEADING_CONFIG, LINK_KEYS_PAGE_CONSTRUCTOR_CONFIG} from './constants';
-import {preprocess} from './preprocess';
+import {ConfigData, PreloadParams, preprocess} from './preprocess';
 
-export type {DocInnerProps, DocPageData, DocLeadingPageData, DocAnalytics};
+export type {
+    DocInnerProps,
+    DocPageData,
+    DocLeadingPageData,
+    DocAnalytics,
+    ConfigData,
+    PreloadParams,
+};
 export type {
     SearchConfig as ISearchProviderConfig,
     WorkerConfig as ISearchWorkerConfig,
