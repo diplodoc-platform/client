7da77aa93cc68a998750bf6a75982595d7480853
Pavel <87060818+Pavel-Tyan@users.noreply.github.com>
2025-03-19 18:36:01 +0300

    feat: add UserSettingManager (#114)
    * feat: add an object for working with localStorage
    
    * feat: add typing for localStorage wrapper object
    
    * fix: change type checking in localStorage wrapper
    
    * feat: add UserSettingManager class
    
    * feat: remove filter from settingsNames

---
+++ src/storage/local.ts
@@ -0,0 +1,41 @@
+/* eslint-disable no-console */
+export default {
+    set: (key: string, payload: unknown) => {
+        try {
+            return localStorage.setItem(key, JSON.stringify(payload));
+        } catch (error) {
+            console.error('Error setting localstorage item', error);
+        }
+    },
+
+    get: (key: string) => {
+        try {
+            const value = localStorage.getItem(key);
+
+            if (value !== null) {
+                return JSON.parse(value);
+            }
+
+            console.warn(`Item with key "${key}" not found in localStorage.`);
+            return null;
+        } catch (error) {
+            console.error(`Error retrieving item from localstorage`, error);
+        }
+    },
+
+    remove: (key: string) => {
+        try {
+            return localStorage.removeItem(key);
+        } catch (error) {
+            console.error('Error removing localstorage item', error);
+        }
+    },
+
+    clear: () => {
+        try {
+            return localStorage.clear();
+        } catch (error) {
+            console.error('Error clearing localstorage', error);
+        }
+    },
+};
