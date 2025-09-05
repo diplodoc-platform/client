08486e73c52dcd9cef1452cbdb8c339e1e890b17
Nikita Lokhmatov <nikitalochmatov@gmail.com>
2025-09-05 14:04:29 +0300

    feat: adding theme provider in search page (#191)
    * feat: adding theme provider in search page
    
    * fix: search page has been converted to the app page view
    
    * fix: fixed search page providers

--- src/search.tsx
+++ src/search.tsx
@@ -2,11 +2,8 @@ import type {SearchConfig} from './components/Search';
 
 import React from 'react';
 import {createRoot} from 'react-dom/client';
-import {configure as configureUikit} from '@gravity-ui/uikit';
-import {configure as configureDocs} from '@diplodoc/components';
 
-import {Search, SearchProvider} from './components/Search';
-import {RouterProvider} from './components/Router';
+import {Search} from './components/Search';
 
 const root = document.getElementById('root');
 
@@ -14,28 +11,15 @@ if (!root) {
     throw new Error('Root element not found!');
 }
 
-const url = new URL(window.location.href);
-
-const windowData = window.__DATA__ as SearchConfig;
-const searchConfig = windowData || {};
-const searchData = windowData.search;
-
-const lang = searchData?.lang;
-
-configureUikit({lang});
-configureDocs({lang});
-
+const data = window.__DATA__ as SearchConfig;
+const searchConfig = data || {};
 searchConfig.depth = searchConfig.depth || 3;
 
-const routerConfig = {
-    pathname: url.pathname,
-    depth: 3,
+const url = new URL(window.location.href);
+
+const props = {
+    data,
+    url,
 };
 
-createRoot(root).render(
-    <RouterProvider value={routerConfig}>
-        <SearchProvider value={searchData}>
-            <Search />
-        </SearchProvider>
-    </RouterProvider>,
-);
+createRoot(root).render(<Search {...props} />);
